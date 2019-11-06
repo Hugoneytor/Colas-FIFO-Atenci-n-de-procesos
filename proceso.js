4ecde0a8d3ad6175b0027ff1a080325a91354b92
hugo3 <halvarez0@ucol.mx>
2019-11-05 23:21:25 -0600

    terminado

--- proceso.js
+++ proceso.js
@@ -1,8 +1,29 @@
 export default class Proceso{
-    constructor(){    
+    constructor(nombre, numCiclos){
+        this._nombre = nombre;
+        this._numCiclos = numCiclos;
+        this._siguiente = null;
     }
-    dado(){
-        let d = Math.trunc(Math.random()*100)+1;
-        return d;
+
+    //Getters
+    get nombre(){
+        return this._nombre;
+    }
+    get numCiclos(){
+        return this._numCiclos;
+    }
+    get siguiente(){
+        return this._siguiente;
+    }
+
+    //Setters
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+    set numCiclos(numCiclos){
+        this._numCiclos = numCiclos;
+    }
+    set siguiente(siguiente){
+        this._siguiente = siguiente;
     }
 }
